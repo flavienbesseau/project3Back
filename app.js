755648f964594bed594ea75d5849bd84c8108742
FlavienBesseau <besseau.flavien@gmail.com>
2020-12-20 16:28:59 +0100

    controllers done

--- app.js
+++ app.js
@@ -1,17 +1,24 @@
-const express = require('express');
-const cors = require('cors');
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const { inTestEnv, inProdEnv, SERVER_PORT } = require('./env');
-
+const express = require("express");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const YAML = require("yamljs");
+const { inTestEnv, inProdEnv, SERVER_PORT } = require("./env");
+const { connection } = require("./db");
 const app = express();
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
 
 // docs
 if (!inProdEnv && !inTestEnv) {
-  const swaggerDocument = YAML.load('./docs/swagger.yaml');
-  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+  const swaggerDocument = YAML.load("./docs/swagger.yaml");
+  app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 }
-
 
 // pre-route middlewares
 app.use(cors());
@@ -19,7 +26,37 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // routes
-require('./routes')(app);
+require("./routes")(app);
+
+app.get("/api/hospitals", (req, res) => {
+  connection.query("SELECT * from ms_hospital", (err, results) => {
+    if (err || results.length === 0) {
+      res.status(500).send("Error retrieving data");
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+app.get("/api/specialties", (req, res) => {
+  connection.query("SELECT * from ms_specialty", (err, results) => {
+    if (err || results.length === 0) {
+      res.status(500).send("Error retrieving data");
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
+
+app.get("/api/experiences", (req, res) => {
+  connection.query("SELECT * from ms_experience", (err, results) => {
+    if (err || results.length === 0) {
+      res.status(500).send("Error retrieving data");
+    } else {
+      res.status(200).json(results);
+    }
+  });
+});
 
 // server setup
 const server = app.listen(SERVER_PORT, () => {
--- db.js
+++ db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const {
   inTestEnv,
   DB_HOST,
@@ -6,7 +6,14 @@ const {
   DB_USER,
   DB_PASS,
   DB_NAME,
-} = require('./env');
+} = require("./env");
+
+console.log("inTestEnv", inTestEnv);
+console.log("DB_HOST", DB_HOST);
+console.log("DB_PORT", DB_PORT);
+console.log("DB_USER", DB_USER);
+console.log("DB_PASS", DB_PASS);
+console.log("DB_NAME", DB_NAME);
 
 class Database {
   init() {
@@ -47,9 +54,9 @@ class Database {
 
   async deleteAllData() {
     if (!inTestEnv)
-      throw new Error('Cannot truncate all table if not in test env !');
+      throw new Error("Cannot truncate all table if not in test env !");
     const truncates = await this.getTableNames().then((tableNames) =>
-       tableNames.map((name) => `TRUNCATE \`${name}\`;`).join(' ')
+      tableNames.map((name) => `TRUNCATE \`${name}\`;`).join(" ")
     );
     const sql = `SET FOREIGN_KEY_CHECKS=0; ${truncates} SET FOREIGN_KEY_CHECKS=1;`;
     return this.query(sql);
@@ -60,7 +67,7 @@ class Database {
       this.tableNames = (
         await this.query(
           `SELECT table_name FROM information_schema.tables where LOWER(table_schema) = '${
-            process.env.DB_NAME_TEST || 'contact_api_database_test'
+            process.env.DB_NAME_TEST || "contact_api_database_test"
           }' AND table_name != 'migrations'`
         )
       ).map((row) => row.table_name || row.TABLE_NAME);
