ca7b61e18c1dccefafa7961f21c32736107833b5
FlavienBesseau <besseau.flavien@gmail.com>
2020-12-29 20:18:00 +0100

    presurvey leads to the good survey

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+
 const swaggerUi = require("swagger-ui-express");
 const YAML = require("yamljs");
 const { inTestEnv, inProdEnv, SERVER_PORT } = require("./env");
 const { connection } = require("./db");
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 connection.connect((err) => {
   if (err) {
@@ -69,16 +73,75 @@ app.get("/api/questions", (req, res) => {
   });
 });
 
-app.get("/api/surveys/:id", (req, res) => {
+// app.get("/api/surveys/:id", (req, res) => {
+//   connection.query(
+//     "SELECT q.text_rating, q.text_comment FROM ms_question q LEFT JOIN ms_question_order o ON q.id = o.fk_question_id WHERE o.fk_survey_id = ?",
+//     [req.params.id],
+//     (err, results) => {
+//       if (err || results.length === 0) {
+//         console.log(err);
+//         res.status(500).send("Error retrieving data");
+//       } else {
+//         res.status(200).json(results);
+//       }
+//     }
+//   );
+// });
+
+app.get("/api/survey", (req, res) => {
+  // let hospitalId = req.query.hospitalId;
+  // let specialtyId = req.query.specialtyId;
+  const { experienceId } = req.query;
+  // console.log(hospitalId);
+  // console.log(specialtyId);
+  console.log(experienceId);
+  if (typeof experienceId === "undefined") {
+    res
+      .status(400)
+      .send("Query param 'experienceId' is required, but received 'undefined'");
+  } else {
+    connection.query(
+      "SELECT q.text_rating, q.text_comment FROM ms_question q LEFT JOIN ms_question_order o ON q.id = o.fk_question_id LEFT JOIN ms_survey s ON s.id = o.fk_survey_id WHERE s.fk_experience_id = ?",
+      [experienceId],
+      (err, results) => {
+        if (err || results.length === 0) {
+          console.log(err);
+          res.status(500).send("Error retrieving data");
+        } else {
+          res.status(200).json(results);
+        }
+      }
+    );
+  }
+});
+
+// un controler avec comme para (lhopital service+type dhospit) et qui retourne le bon survey
+
+app.post("/api/surveys/responses", (req, res) => {
+  const {
+    score,
+    text_answer,
+    fk_question_id,
+    fk_user_id,
+    fk_hospital_id,
+    fk_speciality_id,
+  } = req.body;
   connection.query(
-    "SELECT q.text_rating, q.text_comment FROM ms_question q LEFT JOIN ms_question_order o ON q.id = o.fk_question_id WHERE o.fk_survey_id = ?",
-    [req.params.id],
+    "INSERT INTO ms_response(score, text_answer, fk_question_id, fk_user_id, fk_hospital_id, fk_speciality_id) VALUES(?, ?, ?, ?, ?, ?)",
+    [
+      score,
+      text_answer,
+      fk_question_id,
+      fk_user_id,
+      fk_hospital_id,
+      fk_speciality_id,
+    ],
     (err, results) => {
-      if (err || results.length === 0) {
+      if (err) {
         console.log(err);
-        res.status(500).send("Error retrieving data");
+        res.status(500).send("Error saving a survey");
       } else {
-        res.status(200).json(results);
+        res.status(200).send(results);
       }
     }
   );
