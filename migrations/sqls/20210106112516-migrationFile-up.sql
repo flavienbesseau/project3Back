4dc7609b33ba683b342c47762430165d3ff2a365
FlavienBesseau <besseau.flavien@gmail.com>
2021-01-06 16:09:07 +0100

    updated migration

--- migrations/sqls/20210106112516-migrationFile-up.sql
+++ migrations/sqls/20210106112516-migrationFile-up.sql
@@ -16,14 +16,41 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `ms_experience`
+--
+
+DROP TABLE IF EXISTS `ms_experience`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ms_experience` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `ms_experience`
 --
 
 LOCK TABLES `ms_experience` WRITE;
 /*!40000 ALTER TABLE `ms_experience` DISABLE KEYS */;
-INSERT INTO `ms_experience` VALUES (1,'Hospitalisation avec Bloc'),(2,'Hospitalisation sans Bloc'),(3,'Ambulatoire sans Bloc'),(4,'Ambulatoire avec Bloc'),(5,'Urgences'),(6,'Maternité');
 /*!40000 ALTER TABLE `ms_experience` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `ms_hospital`
+--
+
+DROP TABLE IF EXISTS `ms_hospital`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ms_hospital` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `ms_hospital`
@@ -31,9 +58,28 @@ UNLOCK TABLES;
 
 LOCK TABLES `ms_hospital` WRITE;
 /*!40000 ALTER TABLE `ms_hospital` DISABLE KEYS */;
-INSERT INTO `ms_hospital` VALUES (1,'Hopital de Purpan'),(2,'Hopital de Rangueil'),(3,'Clinique Pasteur'),(4,'Hopital Saint Louis');
 /*!40000 ALTER TABLE `ms_hospital` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `ms_question`
+--
+
+DROP TABLE IF EXISTS `ms_question`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ms_question` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fk_hospital_id` int DEFAULT NULL,
+  `text_rating` varchar(120) DEFAULT NULL,
+  `scale_min` int DEFAULT NULL,
+  `scale_max` int DEFAULT NULL,
+  `text_comment` varchar(1500) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_ms_question_ms_hospital1_idx` (`fk_hospital_id`),
+  CONSTRAINT `fk_ms_question_ms_hospital1` FOREIGN KEY (`fk_hospital_id`) REFERENCES `ms_hospital` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=32 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `ms_question`
@@ -45,6 +91,26 @@ LOCK TABLES `ms_question` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `ms_question_order`
+--
+
+DROP TABLE IF EXISTS `ms_question_order`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ms_question_order` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `order` int NOT NULL,
+  `fk_question_id` int NOT NULL,
+  `fk_survey_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_ms_question_has_ms_survey_ms_survey1_idx` (`fk_survey_id`),
+  KEY `fk_ms_question_has_ms_survey_ms_question1_idx` (`fk_question_id`),
+  CONSTRAINT `fk_ms_question_has_ms_survey_ms_question1` FOREIGN KEY (`fk_question_id`) REFERENCES `ms_question` (`id`),
+  CONSTRAINT `fk_ms_question_has_ms_survey_ms_survey1` FOREIGN KEY (`fk_survey_id`) REFERENCES `ms_survey` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=58 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `ms_question_order`
 --
 
@@ -52,6 +118,34 @@ LOCK TABLES `ms_question_order` WRITE;
 /*!40000 ALTER TABLE `ms_question_order` DISABLE KEYS */;
 /*!40000 ALTER TABLE `ms_question_order` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `ms_response`
+--
+
+DROP TABLE IF EXISTS `ms_response`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ms_response` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `score` int DEFAULT NULL,
+  `text_answer` varchar(45) DEFAULT NULL,
+  `fk_question_id` int NOT NULL,
+  `pseudo` varchar(45) NOT NULL,
+  `fk_hospital_id` int NOT NULL,
+  `fk_specialty_id` int NOT NULL,
+  `post_date` date NOT NULL,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_ms_response_ms_question1_idx` (`fk_question_id`,`pseudo`),
+  KEY `fk_ms_response_ms_hospital1_idx` (`fk_hospital_id`,`pseudo`,`fk_question_id`),
+  KEY `fk_ms_response_ms_user1_idx` (`pseudo`),
+  KEY `fk_ms_response_1_idx` (`fk_specialty_id`),
+  CONSTRAINT `fk_ms_hospital_1` FOREIGN KEY (`fk_hospital_id`) REFERENCES `ms_hospital` (`id`),
+  CONSTRAINT `fk_ms_question_1` FOREIGN KEY (`fk_question_id`) REFERENCES `ms_question` (`id`),
+  CONSTRAINT `fk_ms_specialty_1` FOREIGN KEY (`fk_specialty_id`) REFERENCES `ms_specialty` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
+) ENGINE=InnoDB AUTO_INCREMENT=235 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `ms_response`
@@ -63,6 +157,20 @@ LOCK TABLES `ms_response` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `ms_specialty`
+--
+
+DROP TABLE IF EXISTS `ms_specialty`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ms_specialty` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `ms_specialty`
 --
 
@@ -70,6 +178,25 @@ LOCK TABLES `ms_specialty` WRITE;
 /*!40000 ALTER TABLE `ms_specialty` DISABLE KEYS */;
 /*!40000 ALTER TABLE `ms_specialty` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `ms_specialty_has_ms_experience`
+--
+
+DROP TABLE IF EXISTS `ms_specialty_has_ms_experience`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ms_specialty_has_ms_experience` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `fk_specialty_id` int NOT NULL,
+  `fk_experience_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_ms_specialty_has_ms_experience_ms_experience1_idx` (`fk_experience_id`,`fk_specialty_id`),
+  KEY `fk_ms_specialty_idx` (`fk_specialty_id`),
+  CONSTRAINT `fk_ms_experience` FOREIGN KEY (`fk_experience_id`) REFERENCES `ms_experience` (`id`),
+  CONSTRAINT `fk_ms_specialty` FOREIGN KEY (`fk_specialty_id`) REFERENCES `ms_specialty` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `ms_specialty_has_ms_experience`
@@ -81,6 +208,23 @@ LOCK TABLES `ms_specialty_has_ms_experience` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `ms_survey`
+--
+
+DROP TABLE IF EXISTS `ms_survey`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ms_survey` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `fk_experience_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_ms_survey_ms_experience1_idx` (`fk_experience_id`),
+  CONSTRAINT `fk_ms_survey_ms_experience1` FOREIGN KEY (`fk_experience_id`) REFERENCES `ms_experience` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `ms_survey`
 --
 
@@ -88,6 +232,30 @@ LOCK TABLES `ms_survey` WRITE;
 /*!40000 ALTER TABLE `ms_survey` DISABLE KEYS */;
 /*!40000 ALTER TABLE `ms_survey` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Table structure for table `ms_user`
+--
+
+DROP TABLE IF EXISTS `ms_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ms_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `created_account` datetime DEFAULT NULL,
+  `fk_user_role_id` int NOT NULL,
+  `fk_hospital_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `id_UNIQUE` (`id`),
+  KEY `fk_ms_user_ms_user_role_idx` (`fk_user_role_id`),
+  KEY `fk_ms_user_ms_hospital1_idx` (`fk_hospital_id`),
+  CONSTRAINT `fk_ms_user_ms_hospital1` FOREIGN KEY (`fk_hospital_id`) REFERENCES `ms_hospital` (`id`),
+  CONSTRAINT `fk_ms_user_ms_user_role` FOREIGN KEY (`fk_user_role_id`) REFERENCES `ms_user_role` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `ms_user`
@@ -99,6 +267,21 @@ LOCK TABLES `ms_user` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `ms_user_role`
+--
+
+DROP TABLE IF EXISTS `ms_user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ms_user_role` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `role` varchar(100) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `ms_user_role`
 --
 
@@ -106,6 +289,10 @@ LOCK TABLES `ms_user_role` WRITE;
 /*!40000 ALTER TABLE `ms_user_role` DISABLE KEYS */;
 /*!40000 ALTER TABLE `ms_user_role` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'hospitalidee'
+--
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -116,4 +303,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-01-06 14:18:01
+-- Dump completed on 2021-01-06 14:21:18
