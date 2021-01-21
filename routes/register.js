2287fda5128c2a2910944aa3523fc2644c28a963
Alexandre-Dietsch <dietschalex@gmail.com>
2021-01-21 10:39:22 +0100

    middleware check account

--- routes/register.js
+++ routes/register.js
@@ -3,8 +3,9 @@ const { registerController } = require('../controllers');
 
 const userSchema  = require('../validations/user');
 const validation = require('../middlewares/userValidation');
+const checkAccount = require('../middlewares/checkAccount');
 
 router.get('/', registerController.getUsers);
-router.post('/', validation(userSchema), registerController.createUser);
+router.post('/', checkAccount, validation(userSchema), registerController.createUser);
 
 module.exports = router;
