26eb30084ab613b36b9eb1510cbe03d670d8cb19
Alexandre-Dietsch <dietschalex@gmail.com>
2021-02-01 11:28:42 +0100

    token corrections added

--- middlewares/checkToken.js
+++ middlewares/checkToken.js
@@ -1,19 +1,19 @@
 const jwt = require('jsonwebtoken');
 
-const checkToken = async (req, res, next) => {
-  
+const checkToken = (req, res, next) => {
   const tokenCookie = req.cookies.authcookie;
-  
-  if(tokenCookie === null || tokenCookie === undefined ) {
-    res.status(401).json('You need to provide a valid token');
-  }  
-  jwt.verify(tokenCookie, process.env.JWT_SECRET, (err, user) => {
-    if (err) {
-      res.sendStatus(403);
-    }
-    req.user = user
-    next();
-  })
+
+  if(tokenCookie) {
+    jwt.verify(tokenCookie, process.env.JWT_SECRET, (err, user) => {
+      if(err) {
+        res.status(403).send('Token is incorrect.');
+      }
+      req.user = user;
+       next();
+    })
+  } else {
+     res.status(401).send('You need to provide a valid token');
+  }
 }
 
 module.exports = {
