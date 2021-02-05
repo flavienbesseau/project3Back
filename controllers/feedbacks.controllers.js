25ae1c21d7d6653b5982a26b108282f408a78cce
Alexandre-Dietsch <dietschalex@gmail.com>
2021-02-05 11:45:26 +0100

    change connexion to pool

--- controllers/feedbacks.controllers.js
+++ controllers/feedbacks.controllers.js
@@ -1,9 +1,9 @@
-const connection = require("../db");
+const { pool } = require("../db");
 const { getFeedbacksModels } = require('../models/feedbacks.models');
 
 const getAllFeedbacks = async (req, res) => {
   try {
-    const [rows] = await connection.query(`SELECT * FROM ms_response`);
+    const [rows] = await pool.promise().query(`SELECT * FROM ms_response`);
     res.status(200).json(rows);
   } catch(err) {
     console.log(new Date(), req.method, req.originalUrl, err.message);
@@ -14,7 +14,7 @@ const getAllFeedbacks = async (req, res) => {
 const getFeedbacksByQuestion = async (req, res) => {
   try {
     const { hospitalid, questionid } = req.body;
-    const [rows] = await connection.query(getFeedbacksModels, [hospitalid, questionid])
+    const [rows] = await pool.promise().query(getFeedbacksModels, [hospitalid, questionid])
     res.status(200).json(rows)
   } catch(err) {
     console.log(new Date(), req.method, req.originalUrl, err.message);
